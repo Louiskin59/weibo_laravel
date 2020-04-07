2f0a05409d71b15e2dee30c5ae956092be71e74b
Louiskin59 <1158033392@qq.com>
2020-04-07 16:11:03 +0000

    »ù´¡ҳÃæ

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * 配置三个静态页面路由
+ * 主页、帮助页、关于页
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
