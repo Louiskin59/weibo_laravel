51473e3201bcf77c3684d5a51dcd5657ae3e3283
Louiskin59 <1158033392@qq.com>
2020-04-15 15:00:03 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,5 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('signup', 'UserController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
